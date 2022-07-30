0af4237b9e0580c8d20a97093e1b514c0a8c3f3b
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-07-30 20:16:01 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+startBtnEl.addEventListener('click', OnBodyStartColorChange);
+stopBtnEl.addEventListener('click', OnBodyStopColorChange);
+
+function OnBodyStartColorChange(evt) {
+    timerID = setInterval(addBodyColor, 1000);
+    startBtnEl.disabled = true;
+};
+
+function OnBodyStopColorChange(evt) {
+    clearInterval(timerID);
+    startBtnEl.disabled = false;
+};
+
+function addBodyColor(){
+    bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
+console.log(getRandomHexColor())
