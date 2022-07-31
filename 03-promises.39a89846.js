3c736d7676c3f90c776577edc13a17578cbe7ce0
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-07-31 18:32:42 +0000

    Deploying to gh-pages from @ SerhiiIsaev/goit-js-hw-09@4c16c348480ab1d8ac28bac3d5c6c73e1b7ebad8 🚀

---
+++ 03-promises.39a89846.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("form"),t=document.querySelector("button");t.addEventListener("submit",(function(t){t.preventDefault();t.currentTarget.elements;console.log(t.currentTarget.elements),e.reset()})),console.log(t)}();
+//# sourceMappingURL=03-promises.39a89846.js.map
---
+++ 03-promises.d1038770.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("form"),t=document.querySelector("button");t.addEventListener("submit",(function(t){t.preventDefault();t.currentTarget.elements;console.log(t.currentTarget.elements),e.reset()})),console.log(t);
+//# sourceMappingURL=03-promises.d1038770.js.map
