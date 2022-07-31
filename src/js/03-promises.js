4c16c348480ab1d8ac28bac3d5c6c73e1b7ebad8
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-07-31 21:31:54 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const formEl = document.querySelector("form");
+const createPromisesBtn = document.querySelector("button");
+
+createPromisesBtn.addEventListener('submit', onBtnCreatePromises)
+console.log(createPromisesBtn);
+const formElements = {}
+
+
+function onBtnCreatePromises(evt) {
+  evt.preventDefault();
+  const formElements = evt.currentTarget.elements
+  console.log(evt.currentTarget.elements);
+  formEl.reset()
 }
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
