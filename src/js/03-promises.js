cc4ea1bf2f78d3c706bcd0c89f3b15ddc6d5ed5e
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-08-01 22:29:49 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+
 const formEl = document.querySelector("form");
 const createPromisesBtn = document.querySelector("button");
 
@@ -5,41 +6,34 @@ formEl.addEventListener('submit', onBtnCreatePromises)
 console.log(createPromisesBtn);
 const formElements = {}
 
-
 function onBtnCreatePromises(evt) {
   evt.preventDefault();
-  const formElements = evt.currentTarget.elements
+  const formElements = evt.currentTarget.elements;
+  console.log(Number(formElements.delay.value));
+  let timer = Number(formElements.delay.value);
   for (let i = 0; i < Number(formElements.amount.value); i += 1 ){
-    createPromise(i+1, Number(formElements.step.value))
+    delayMessage(i, timer)
+    timer += Number(formElements.step.value);
   }
-  console.log(Number(formElements.amount.value));
   formEl.reset()
 }
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
 
-function createPromise(position, delay){
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+function delayMessage(i, timer) {
+  return new Promise( (resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      setTimeout(()=> {resolve({i, timer})} ,timer );
+    } else {
+      setTimeout(() => { reject({i, timer})}, timer);
+    }
+    
+
+}).then(({ i, timer }) => {
+    console.log(`✅ Fulfilled promise ${i} in ${timer}ms`);
+  })
+  .catch(({ i, timer }) => {
+    console.log(`❌ Rejected promise ${i} in ${timer}ms`);
+  });
+ 
 }
-
-// function createPromise(position, delay){
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   } else {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   }
-// }
-
